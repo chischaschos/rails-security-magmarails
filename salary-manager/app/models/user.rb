1faf953917582ef1e897f785381ec59b5ab1ac01
Emmanuel Delgado <emmanuel.delgado@crowdint.com>
2010-09-18 20:02:20 -0500

    Wicho helped me fix a create users error

--- salary-manager/app/models/user.rb
+++ salary-manager/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :role_id, :salary
   has_one :salary
   accepts_nested_attributes_for :salary
   belongs_to :role
